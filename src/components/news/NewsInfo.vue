4e0798425e749a16afe00bf0a432ffcca31b97ec
gaoqian113242 <49133772+gaoqian113242@users.noreply.github.com>
2019-06-02 11:46:09 +0800

    a

--- src/components/news/NewsInfo.vue
+++ src/components/news/NewsInfo.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class="newsinfo-container">
-        <h3 class="title" v-if="id=$route.params.id">{{ newsinfo.title }}</h3>
+    <div class="newsinfo-container" >
+        <li v-for="item in newsinfo" :key="item.id" >
+        <h3 class="title" >{{ item.title }}</h3>
         <p class="subtitle">
-            <span>发表时间: {{ newsinfo.add_time | dateFormat }}</span>
-            <span>点击：{{ newsinfo.click  }}次</span>
+            <span>发表时间: {{ item.add_time | dateFormat }}</span>
+            <span>点击：{{ item.click  }}次</span>
         </p>
 
         <hr>
 
         <div class="content"></div>
-
+</li>
         <comment-box></comment-box>
     </div>
 </template>
@@ -21,34 +22,33 @@ export default {
     data(){
         return{
             id: this.$route.params.id,
-            newsinfo: {}
+            newsinfo: [
+        //         {
+        //         id: 13,
+        //         title : "买房还是炒股，2015年买房无法拒绝的5大理由",
+        //         click : 1,
+        //         add_time:  "2019-05-27 10:10:10",
+        //         content : "https://img10.360buyimg.com/n7/jfs/t1/31203/23/9994/197564/5cab210dE64e20358/d94e70571c1fd455.jpg"
+        // }
+            ]
         };
     },
     created() {
-        this.getNewsList();
+        this.getNewsInfo();
     },
     methods: {
-        // getNewsInfo(){
-        //     this.$http.get("static/newsinfo.json").then(
-        //     result => {
-        //         var data = result.body;
-        //         this.newsinfo = data.message;
-        //         console.log(this.newsinfo)
-        //     },
-        //     error => {
-        //         Toast("失败");
-        //     })
-        // },
-        getNewsList(){
-            this.$http.get("static/newslist.json").then(
+        getNewsInfo(){
+            this.$http.get("static/newsinfo.json").then(
             result => {
                 var data = result.body;
                 this.newsinfo = data.message;
+                console.log(this.newsinfo)
             },
             error => {
                 Toast("失败");
             })
-        }
+        },
+        
     },
     components: {
         "comment-box": comment
@@ -57,6 +57,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+li{
+    margin: 0;
+}
 .newsinfo-container{
     padding: 0 4px;
     .title{
